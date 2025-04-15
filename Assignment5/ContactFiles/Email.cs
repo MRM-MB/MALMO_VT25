f007e6e95f28f47faa022695f21d479f04656bab
MRM-MB <manishmoriche@gmail.com>
2025-04-15 16:14:32 +0200

    A5 comments update

--- Assignment5/ContactFiles/Email.cs
+++ Assignment5/ContactFiles/Email.cs
@@ -9,47 +9,46 @@ using System.Threading.Tasks;
 
 public class Email
 {
-    //private email
+    // Private email address
     private string personalMail = string.Empty;
-    //officeMail mail
+    // Office email address
     private string officeMail = string.Empty;
 
-    //Constructors are overloaded and called in a chain
+    // Constructors are overloaded and called in a chain
 
     /// <summary>
-    /// Default constructor - calls another constructor in this class
+    /// Default constructor - initializes fields with default values.
     /// </summary>
-    /// <remarks></remarks>
     public Email()
     {
-        // Fields are now initialized with default values at declaration
+        // Fields are initialized with default values at declaration
     }
 
     /// <summary>
     /// Constructor with one parameter - calls the constructor with 
-    /// two parameters, using a default value as the second argument.
+    /// two parameters, using an empty string as the second argument.
     /// </summary>
-    /// <param name="workMail">input coming from the client object</param>
-    /// <remarks></remarks>
+    /// <param name="workMail">Office email address provided by the client</param>
     public Email(string workMail) : this(workMail, string.Empty)
     {
     }
+
     /// <summary>
-    /// Copy constructor returning copy
+    /// Copy constructor - creates a new Email object with the same values as the provided one.
     /// </summary>
+    /// <param name="theOther">The Email object to copy from</param>
     public Email(Email theOther)
     {
         this.personalMail = theOther.personalMail;
         this.officeMail = theOther.officeMail;
     }
+
     /// <summary>
-    /// Constructor with two parameters. This is  constructor that has most
-    /// number of parameters. It is in tis constructor that all coding 
-    /// should be done.
+    /// Constructor with two parameters. This is the constructor with the most
+    /// parameters and contains the main initialization logic.
     /// </summary>
-    /// <param name="workMail">Input - office mail</param>
-    /// <param name="personalMail">Input - private mail</param>
-    /// <remarks></remarks>
+    /// <param name="workMail">Office email address</param>
+    /// <param name="personalMail">Personal email address</param>
     public Email(string workMail, string personalMail)
     {
         officeMail = workMail;
@@ -57,56 +56,38 @@ public class Email
     }
 
     /// <summary>
-    /// Property related to the field m_Personal
-    /// Both read and write access
+    /// Property for accessing and modifying the personal email address.
     /// </summary>
-    /// <value></value>
-    /// <returns></returns>
-    /// <remarks></remarks>
     public string Personal
     {
-        //private mail
         get { return personalMail; }
-
         set { personalMail = value; }
     }
 
-
     /// <summary>
-    /// Property related to officeMail field
-    /// Both read and write access
+    /// Property for accessing and modifying the work email address.
     /// </summary>
-    /// <value></value>
-    /// <returns></returns>
-    /// <remarks></remarks>
     public string Work
     {
         get { return officeMail; }
-
         set { officeMail = value; }
     }
 
     /// <summary>
-    /// This method prepares a format string that is in sync with the ToString
-    /// method.  It will be used in the MainForm as part of the heading for the ListBox
-    /// before customer information is added in the ListBox.
+    /// Provides a formatted string to be used as a header in the UI.
+    /// This header format matches the data format in the ToString method.
     /// </summary>
-    /// <value></value>
-    /// <returns>A formatted string as heading for the values formatted in the ToString
-    /// method below.</returns>
-    /// <remarks></remarks>
+    /// <returns>A formatted string to be used as a column header in the ListBox</returns>
     public string GetToStringItemsHeadings
     {
         get { return string.Format("{0,-20} {1, -20}", "Office EmailData", "Private EmailData"); }
     }
 
     /// <summary>
-    /// Delivers a formatted string with data stored in the object. The values will
-    /// appear as columns.  Make sure that a font like "Courier New" is used in
-    /// the control displaying this information.
+    /// Returns a formatted string containing the email addresses.
+    /// Use a monospaced font (like "Courier New") for proper column alignment when displaying.
     /// </summary>
-    /// <returns>the Formatted strings.</returns>
-    /// <remarks></remarks>
+    /// <returns>A formatted string representation of the email addresses</returns>
     public override string ToString()
     {
         string strOut = "\n" + "Emails" + "\n";
@@ -116,5 +97,4 @@ public class Email
 
         return strOut;
     }
-
 }
